e9aac6705f10bc3d3735a00878e9043a196d24e4
Noornabi <noornabimd9891@gmail.com>
2025-05-03 07:13:40 +0600

    Added Longest substring without repeating characters using another bruteforce approches complexity O(N^2)

--- Sliding Window/LongestSubstringWithoutRepeatingChar.cpp
+++ Sliding Window/LongestSubstringWithoutRepeatingChar.cpp
@@ -1,40 +1,66 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     string s;
+//     cin>>s;
+
+//     unordered_map<char,int>mp;
+   
+//     int sz=s.size();
+//     int mxL=INT_MIN;
+//     for(int i=0;i<sz;i++){
+       
+//         int c=0;
+       
+//         for(int j=i;j<sz;j++){
+            
+//             unordered_set<char> ch;
+
+//             for(int k=i;k<=j;k++){
+                
+//                 ch.insert(s[k]); 
+                
+//             }
+
+//             if (ch.size() == j - i + 1) {
+//                 mxL = max(mxL, j - i + 1);
+//             }
+            
+//         }
+        
+//     }
+
+//     cout<<mxL<<'\n';
+    
+//     return 0;
+// }
+
+
+//Time:O(n^3)
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
 
 int main(){
     string s;
     cin>>s;
-
-    unordered_map<char,int>mp;
-   
     int sz=s.size();
-    int mxL=INT_MIN;
+    int mxL = 0;
     for(int i=0;i<sz;i++){
-       
-        int c=0;
-       
+        unordered_set<char> ch;
         for(int j=i;j<sz;j++){
-            
-            unordered_set<char> ch;
-
-            for(int k=i;k<=j;k++){
-                
-                ch.insert(s[k]); 
-                
+            if(ch.count(s[j])){
+                break;
             }
-
-            if (ch.size() == j - i + 1) {
-                mxL = max(mxL, j - i + 1);
-            }
-            
+            ch.insert(s[j]);
+            mxL = max(mxL, j - i + 1);
         }
-        
     }
-
-    cout<<mxL<<'\n';
-    
+    cout << mxL << '\n';
     return 0;
 }
 
-
-//Time:O(n^3)
+//O(N^2)
