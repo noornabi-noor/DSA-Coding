1d1aad54b057a18615c261272b7378788e31ccfe
Noornabi <noornabimd9891@gmail.com>
2025-05-03 07:08:28 +0600

    Added Longest substring without repeating characters using bruteforce approches complexity O(N^3)

---
+++ Sliding Window/LongestSubstringWithoutRepeatingChar.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    unordered_map<char,int>mp;
+   
+    int sz=s.size();
+    int mxL=INT_MIN;
+    for(int i=0;i<sz;i++){
+       
+        int c=0;
+       
+        for(int j=i;j<sz;j++){
+            
+            unordered_set<char> ch;
+
+            for(int k=i;k<=j;k++){
+                
+                ch.insert(s[k]); 
+                
+            }
+
+            if (ch.size() == j - i + 1) {
+                mxL = max(mxL, j - i + 1);
+            }
+            
+        }
+        
+    }
+
+    cout<<mxL<<'\n';
+    
+    return 0;
+}
+
+
+//Time:O(n^3)
