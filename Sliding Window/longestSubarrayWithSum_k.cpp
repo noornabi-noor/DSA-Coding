aaa19e6e685e480e53977208ee5bf5f86a55e189
Noornabi <noornabimd9891@gmail.com>
2025-05-16 20:23:27 +0600

    Some update code

--- Sliding Window/longestSubarrayWithSum_k.cpp
+++ Sliding Window/longestSubarrayWithSum_k.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
+
     vector<int> v(n);
     for(int i = 0; i < n; i++) {
         cin >> v[i];
@@ -11,6 +12,7 @@ int main() {
 
     int start = 0, sum = 0, max_len = 0;
     for(int end = 0; end < n; end++) {
+
         sum += v[end];
 
         while(sum > k && start <= end) {
@@ -21,10 +23,13 @@ int main() {
         if(sum == k) {
             max_len = max(max_len, end - start + 1);
         }
+
     }
 
     cout << max_len;
+
     return 0;
 }
+
 
 //O(N)
